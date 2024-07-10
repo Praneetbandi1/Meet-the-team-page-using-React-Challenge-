8669e09bfc82b65c264a25dfe611ba76076bdf05
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-07-09 21:11:39 -0400

    Comitting default Gallery and Profile components

---
+++ team-member-app/src/components/Gallery/Gallery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Gallery = () => (
+  <div>
+    Gallery Component
+  </div>
+);
+
+Gallery.propTypes = {};
+
+Gallery.defaultProps = {};
+
+export default Gallery;
---
+++ team-member-app/src/components/Profile/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Profile = () => (
+  <div>
+    Profile Component
+  </div>
+);
+
+Profile.propTypes = {};
+
+Profile.defaultProps = {};
+
+export default Profile;
